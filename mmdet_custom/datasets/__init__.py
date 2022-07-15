a731ea1ed78fa0e4b34d8a9c8cd867186ad31791
vansin <msnode@163.com>
2022-07-15 08:51:31 +0800

    feat: add xray Dataset

--- mmdet_custom/datasets/__init__.py
+++ mmdet_custom/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .d10185 import D10185Dataset
 from .fire import FireDataset
 from .tabnet import TableDataset
+from .xray import XrayDataset
 
-__all__ = ['TableDataset', 'FireDataset', 'D10185Dataset']
+__all__ = ['TableDataset', 'FireDataset', 'D10185Dataset', 'XrayDataset']
