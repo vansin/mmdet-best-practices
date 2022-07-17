ed86d198fa89cab1950c2398a25a81a892ae9c4e
vansin <msnode@163.com>
2022-07-17 17:50:42 +0800

    feat: add wider_face_coco

--- mmdet_custom/datasets/__init__.py
+++ mmdet_custom/datasets/__init__.py
@@ -1,8 +1,9 @@
 from .tabnet import TableDataset
 from .voc2007_coco import VOC2007CocoDataset
+from .wider_face_coco import WIDERFaceCocoDataset
 from .xray import XrayDataset
 
 __all__ = [
     'TableDataset', 'FireDataset', 'D10185Dataset', 'XrayDataset',
-    'VOC2007CocoDataset'
+    'VOC2007CocoDataset', 'WIDERFaceCocoDataset'
 ]
