68623fea6b31d7f74592fc981c09d00285798709
vansin <msnode@163.com>
2022-09-02 15:09:35 +0800

    feat: add ballon2coco.py

---
+++ configs/balloon/_base_/datasets/coco_detection.py
@@ -0,0 +1,45 @@
+# dataset settings
+dataset_type = 'COCODataset'
+data_root = 'data/'
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
+                    std=[58.395, 57.12, 57.375],
+                    to_rgb=True)
+
+classes = ('balloon', )
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='MultiScaleFlipAug',
+         img_scale=(1333, 800),
+         flip=False,
+         transforms=[
+             dict(type='Resize', keep_ratio=True),
+             dict(type='RandomFlip'),
+             dict(type='Normalize', **img_norm_cfg),
+             dict(type='Pad', size_divisor=32),
+             dict(type='ImageToTensor', keys=['img']),
+             dict(type='Collect', keys=['img']),
+         ])
+]
+
+data = dict(train=dict(img_prefix='balloon/train/',
+                       classes=classes,
+                       ann_file='balloon/train.json'),
+            val=dict(img_prefix='balloon/val/',
+                     classes=classes,
+                     ann_file='balloon/val.json'),
+            test=dict(img_prefix='balloon/val/',
+                      classes=classes,
+                      ann_file='balloon/val.json'))
+
+evaluation = dict(interval=1, metric='bbox')
