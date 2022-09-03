9c48f8670c934aed2e7f46b617f04641138b178e
vansin <msnode@163.com>
2022-09-03 15:32:37 +0800

    [Fix] fix the path

--- configs/balloon/_base_/datasets/coco_instance.py
+++ configs/balloon/_base_/datasets/coco_instance.py
@@ -1,6 +1,6 @@
 # dataset settings
-dataset_type = 'VOC2007CocoDataset'
-data_root = 'data/VOCdevkit/'
+dataset_type = 'CocoDataset'
+data_root = 'data/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
                     std=[58.395, 57.12, 57.375],
                     to_rgb=True)
