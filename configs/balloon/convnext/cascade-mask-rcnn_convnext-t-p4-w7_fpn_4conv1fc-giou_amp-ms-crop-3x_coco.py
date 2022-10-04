ffd6ed866268f757c86115fa842cd9e07535fc6f
vansin <msnode@163.com>
2022-10-04 13:11:36 +0800

    [Feature] update

--- configs/balloon/convnext/cascade-mask-rcnn_convnext-t-p4-w7_fpn_4conv1fc-giou_amp-ms-crop-3x_coco.py
+++ configs/balloon/convnext/cascade-mask-rcnn_convnext-t-p4-w7_fpn_4conv1fc-giou_amp-ms-crop-3x_coco.py
@@ -154,6 +154,6 @@ optim_wrapper = dict(
         betas=(0.9, 0.999),
         weight_decay=0.05))
 
-custom_hooks = [
-    dict(type='MaxValueHook')
-]
+# custom_hooks = [
+#     dict(type='MaxValueHook')
+# ]
