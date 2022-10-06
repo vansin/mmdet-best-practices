c0b10b255eaed6c8b1afca6cd8a43e865b6977b7
vansin <msnode@163.com>
2022-10-06 15:43:57 +0800

    [Feature] update

--- tools/analysis_tools/get_flops.py
+++ tools/analysis_tools/get_flops.py
@@ -1,17 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 
-import numpy as np
 import torch
-from mmengine.config import Config, DictAction
+from fvcore.nn import FlopCountAnalysis, flop_count_table
+from mmengine import Config
 
-from mmdet.models import build_detector
+from mmdet.registry import MODELS
+from mmdet.utils import register_all_modules
+from mmengine.runner import Runner
 
-try:
-    from mmcv.cnn import get_model_complexity_info
-except ImportError:
-    raise ImportError('Please upgrade mmcv to >0.6.2')
-
+register_all_modules()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
@@ -20,24 +18,8 @@ def parse_args():
         '--shape',
         type=int,
         nargs='+',
-        default=[1280, 800],
+        default=[640, 640],
         help='input image size')
-    parser.add_argument(
-        '--cfg-options',
-        nargs='+',
-        action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
-        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
-        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
-    parser.add_argument(
-        '--size-divisor',
-        type=int,
-        default=32,
-        help='Pad the input image, the minimum size that is divisible '
-        'by size_divisor, -1 means do not pad the image.')
     args = parser.parse_args()
     return args
 
@@ -51,43 +33,29 @@ def main():
     elif len(args.shape) == 2:
         h, w = args.shape
     else:
-        raise ValueError('invalid input shape')
-    ori_shape = (3, h, w)
-    divisor = args.size_divisor
-    if divisor > 0:
-        h = int(np.ceil(h / divisor)) * divisor
-        w = int(np.ceil(w / divisor)) * divisor
+        raise ValueError('invalid input shape, please use --shape h w')
 
-    input_shape = (3, h, w)
-
+    input_shape = (1, 3, h, w)
     cfg = Config.fromfile(args.config)
-    if args.cfg_options is not None:
-        cfg.merge_from_dict(args.cfg_options)
 
-    model = build_detector(
-        cfg.model,
-        train_cfg=cfg.get('train_cfg'),
-        test_cfg=cfg.get('test_cfg'))
-    if torch.cuda.is_available():
-        model.cuda()
-    model.eval()
+    # dataloader = Runner.build_dataloader(cfg.val_dataloader)
 
-    if hasattr(model, 'forward_dummy'):
-        model.forward = model.forward_dummy
-    else:
-        raise NotImplementedError(
-            'FLOPs counter is currently not currently supported with {}'.
-            format(model.__class__.__name__))
+    model = MODELS.build(cfg.model)
 
-    flops, params = get_model_complexity_info(model, input_shape)
-    split_line = '=' * 30
+    # for idx, data_batch in enumerate(dataloader):
+    #     print(idx, data_batch)
+    #     break
 
-    if divisor > 0 and \
-            input_shape != ori_shape:
-        print(f'{split_line}\nUse size divisor set input shape '
-              f'from {ori_shape} to {input_shape}\n')
-    print(f'{split_line}\nInput shape: {input_shape}\n'
-          f'Flops: {flops}\nParams: {params}\n{split_line}')
+    # flops = FlopCountAnalysis(model, (torch.ones(input_shape), data_batch['data_samples']))
+    # flops = FlopCountAnalysis(model,{"inputs": data_batch['inputs'],"data_samples": data_batch['data_samples']})
+    # flops = FlopCountAnalysis(model,data_batch['inputs'][0])
+    flops = FlopCountAnalysis(model,torch.ones(input_shape))
+
+    # params = parameter_count_table(model)
+    flops_data = flop_count_table(flops)
+
+    print(flops_data)
+
     print('!!!Please be cautious if you use the results in papers. '
           'You may need to check if all ops are supported and verify that the '
           'flops computation is correct.')
