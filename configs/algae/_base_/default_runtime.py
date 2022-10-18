beb94392aea3b808c5d513f89f48d0134181fe9f
vansin <msnode@163.com>
2022-10-18 15:53:28 +0800

    [Feature] update

--- configs/algae/_base_/default_runtime.py
+++ configs/algae/_base_/default_runtime.py
@@ -4,7 +4,8 @@ default_hooks = dict(
     timer=dict(type='IterTimerHook'),
     logger=dict(type='LoggerHook', interval=50),
     param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(type='CheckpointHook', interval=1),
+    # checkpoint=dict(type='CheckpointHook', interval=1,save_best=['coco/MaxF1Score90'],rule='greater', max_keep_ckpts=1),
+    checkpoint=dict(type='CheckpointHook', interval=1,save_best=['auto'],rule='greater', max_keep_ckpts=1),
     sampler_seed=dict(type='DistSamplerSeedHook'),
     visualization=dict(type='DetVisualizationHook'))
 
